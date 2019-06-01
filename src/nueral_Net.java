85bed2c2c94a8eb45e45a8ba15626fac2720824d
Max-Bus <34168923+Max-Bus@users.noreply.github.com>
2019-06-01 19:50:08 -0400

    bit of an api

---
+++ src/nueral_Net.java
@@ -0,0 +1,40 @@
+import java.awt.image.BufferedImage;
+
+public class nueral_Net {
+    Nueron[][] nuerons;
+    public nueral_Net(int layers){
+
+    }
+    public void savestate(){
+
+    }
+    public void training(/*tests not sure what data type*/){
+
+    }
+    private void backprop(){
+
+    }
+    public int guessthatnumber(BufferedImage img){
+        return -1;
+    }
+
+}
+class Nueron{
+    double value;
+    double[] weights;
+    double[] bias;
+    //maybe it should have a list of nodes from previous layer
+    public Nueron(double val, int previouslayer){
+        weights=new double[previouslayer];
+        bias=new double[previouslayer];
+        value=val;
+    }
+    public double[] requestsweights(double target){ //makes a request for changes to weights given a target value
+        return null;
+    }
+    //public double[] requestsnuerons(double target){} // asks for changes in parent nuerons maybe implemented
+    public void calculateval(){
+        //value= weighted sum of past nodes values plus bias
+    }
+
+}
