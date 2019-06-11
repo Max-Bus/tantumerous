a3525d0f7f25e23143148446f4bbc052c7042505
Max-Bus <34168923+Max-Bus@users.noreply.github.com>
2019-06-10 20:44:43 -0400

    this kinda works

--- src/neural_Net.java
+++ src/neural_Net.java
@@ -9,11 +9,11 @@ import java.util.stream.Stream;
 
 //hello, I changed a thing, you initially spelled "neurons" wrong garcia
 public class neural_Net {
-    private Neuron[][] neurons;
+    public Neuron[][] neurons;
     private int layers;
     public final int width;
     public final int height;
-    public final int outputnodes=10;
+    public final int outputnodes=2;
     public neural_Net(int layers,int w, int h){
         width=w;
         this.layers=layers;
@@ -33,6 +33,7 @@ public class neural_Net {
             }
             for (int j = 0; j < neurons[i].length; j++) {
                 neurons[i][j] = new Neuron(0, neurons[i - 1].length);
+                neurons[i][j].scramble();
             }
 
         }
@@ -81,11 +82,7 @@ public class neural_Net {
                 out.write(':');
 
                 if(neurons[layer][neuron].weights.length > 0) {
-                    for (int bias = 0; bias < neurons[layer][neuron].bias.length - 1; bias++) {
-                        out.write(Double.toString(neurons[layer][neuron].bias[bias]));
-                        out.write(" ");
-                    }
-                    out.write(Double.toString(neurons[layer][neuron].bias[neurons[layer][neuron].bias.length - 1]));
+                        out.write(Double.toString(neurons[layer][neuron].bias));
                 }
 
                 out.write(",");
@@ -110,7 +107,7 @@ public class neural_Net {
         //they are structured as: layer, node, weights/biases
         //*fairies away*
         ArrayList<ArrayList<ArrayList<Double>>> weightsForNodes = new ArrayList<>();
-        ArrayList<ArrayList<ArrayList<Double>>> biasesForNodes = new ArrayList<>();
+        ArrayList<ArrayList<Double>> biasesForNodes = new ArrayList<>();
 
 
         BufferedReader reader = new BufferedReader(new FileReader(datafile));
@@ -147,11 +144,11 @@ public class neural_Net {
                     cachedList = new ArrayList<Double>();                    
                 }
                 else if(line.charAt(character) == ','){
-                    biasesForNodes.get(currentLayer).add(cachedList);
+                    biasesForNodes.get(currentLayer).add(cachedList.get(0));
                     //if we get an error here, change it to length-1
                     lineInterpreter.delete(0,lineInterpreter.length());
-                    cachedList = new ArrayList<Double>();
-           
+                    cachedList = new ArrayList<>();
+
                 }
                 else{
                 lineInterpreter.append(line.charAt(character));
@@ -175,12 +172,9 @@ public class neural_Net {
                 for(int i = 0; i < newWeights.length; i++){
                     newWeights[i] = weightsForNodes.get(layer).get(neuron).get(i);
                 }
-                double[] newBiases = new double[biasesForNodes.get(layer).get(neuron).size()];
-                for(int r = 0; r < newBiases.length; r++){
-                    newBiases[r] = biasesForNodes.get(layer).get(neuron).get(r);
-                }
 
-                neurons[layer][neuron] = new Neuron(newWeights,newBiases,-50);
+
+                neurons[layer][neuron] = new Neuron(newWeights,biasesForNodes.get(layer).get(neuron),-50);
             }
         }
     }
@@ -198,94 +192,108 @@ public class neural_Net {
         String[] files;
 
     }
-    public void sumofweights(int expoutput,double[][][] aggregate){
-        double[] requests=cost(expoutput);
-        for (int i = layers-1; i >1 ; i++) {
-            aggregate[i]=new double[neurons[i].length][neurons[i-1].length];
-            for (int j = 0; j <neurons[i].length; j++) {
-                for (int k = 0; k <neurons[i-1].length ; k++) {
-                    aggregate[i][j][k]+=neurons[i][j].requestsweights(requests[j],neurons[i-1])[k];
-                }
-            }
-            requests=neuronchanges(i-1,requests);
-        }
-    }
-
-    public double[] neuronchanges(int layer,double[] changes){
-        //finds what the average requests for neurons of the previuos layer is
-        double[] request=new double[neurons[layer].length];
-        for (int i = 0; i <neurons[layer+1].length ; i++) {
-            for (int j = 0; j <neurons[layer].length ; j++) {
-                request[j]+=neurons[layer+1][i].requestsneurons(changes[i],neurons[layer])[j];
-            }
-        }
-        for (int i = 0; i <request.length ; i++) {
-            request[i]/=neurons[layer].length;
-        }
-        //average the requestes made by nuerons of previous layer
-        return request;
-    }
-    public double[] cost(double expout){
+    public double cost(double[] expout){
         //base requests based off expected output
-        double[] out=new double[outputnodes];
+        double out=0;
         for (int i = 0; i <outputnodes ; i++) {
-            if(i==expout){
-                out[i]=1-neurons[layers-1][i].getValue();
-            }
-            else{
-                out[i]=neurons[layers-1][i].getValue()-1;
-            }
+            out+=(neurons[layers-1][i].getValue()-expout[i])*(neurons[layers-1][i].getValue()-expout[i]);
         }
         return out;
     }
-
-    private void backprop(String[] imgs,int[] expectedOutput,int numloops)throws IOException{
+    public void singlebackprop(double[][][] weights,double[][]bias,double[] expout,int layer,double learningrate){
+        weights[layer]=new double[neurons[layer].length][neurons[layer-1].length];
+        bias[layer]=new double[neurons[layer].length];
+        for (int i = 0; i <outputnodes ; i++) {
+            Neuron n=neurons[layer][i];
+            double costoverbias = learningrate*(expout[i]-n.getValue());
+            bias[layer][i]+=costoverbias;
+            for (int j = 0; j <neurons[layer-1].length ; j++) {
+                double costoverweight = learningrate* neurons[layer - 1][j].getValue()*(expout[i]-n.getValue());
+                weights[layer][i][j] += costoverweight;
+            }
+        }
+    }
+    public double sumcost(double[] expout,int layer){
+        double val=0;
+        for (int i = 0; i <neurons[layer].length ; i++) {
+            val+=2*(neurons[layer][i].getValue()-expout[i]);
+        }
+        return val;
+    }
+    public void backprop(String[] imgs,Integer[] expectedOutput)throws IOException{
         File f=null;
         BufferedImage img=null;
-        double[][][] aggregateWeights=new double[layers][][]; // array full of all requested weight changes
-        for(int j=0;j<numloops;j++) {
+        double learningrate =2;
+        double percent=0;
+        double oldpercent=0;
+        while ((percent/imgs.length)*100<93){
+            double[][][] aggregateWeights=new double[layers][][]; // array full of all requested weight changes
+            double[][] aggregateBiases=new double[layers][];
             for (int i = 0; i < imgs.length; i++) {
                 //its going to run through a training example find out the weights and nodes that need to be strengthened
-                f = new File("Images\\" + imgs[i] + ".png");
-                img = ImageIO.read(f);
-                guessthatnumber(img); //runs teh img through the neural net
+                f = new File("data_set/" + imgs[i]);
+                try {
+                    img = ImageIO.read(f);
+                }catch (Exception e){
+                    System.out.println(imgs[i]);
+                }
+                tantum_Numerus_Main.grayScale(img);
+                BufferedImage resizedimg=tantum_Numerus_Main.resize(img);
+                double[] num = guessthatnumber(resizedimg); //runs teh img through the neural net
                 //find what needs to happen
-                sumofweights(expectedOutput[j],aggregateWeights); //propigates backwards through the neural net adding requests to aggregatedWeights
-                //for each neuron add to the weights the absolute value some number(amount it ust be changed times the value of the proceeding neuron
-                //request decreases in the neurons of the previus layer by multiplying some number by teh weights associated with those neurons
+                if((int)num[0]==expectedOutput[i]){
+                    percent++;
+                }
+                double[] out=new double[outputnodes];
+                for (int k = 0; k <outputnodes ; k++) {
+                    if(k==expectedOutput[i]){
+                        out[k]=1;
+                    }
+                    else {
+                        out[k]=0;
+                    }
+                }
+                if(layers==2){
+                    singlebackprop(aggregateWeights,aggregateBiases,out,1,learningrate);
+                }
 
-
-                // call a recursive (maybe normal loop instead) function that repeats the process for each layer
-
-                // then it will store that info
             }
             //averages the value of each weight that was requested
             //first list is list of layers second list is list of neurons third list is the list of weights in said neuron
-            for (double[][] layer:aggregateWeights) {
-                for (double[] weights:layer) {
-                    for (int i = 0; i <weights.length ; i++) {
-                        weights[i]/=imgs.length;
-                    }
-                }
 
+            //finally it will rewrite each neuron to match the average
+            for (int i =1;i<aggregateWeights.length;i++) {
+                    for (int k=0;k<aggregateWeights[i].length;k++) {
+                        for (int l = 0; l <aggregateWeights[i][k].length; l++) {
+                            aggregateWeights[i][k][l] /= imgs.length;
+
+                        }
+                        aggregateBiases[i][k]/= imgs.length;
+                    }
             }
             //takes the averaged list and change neurons accordingly
-            for (int i = 0; i <layers ; i++) {
-                for (int k = 0; k <neurons[k].length ; k++) {
+            for (int i = 1; i <layers ; i++) {
+                for (int k = 0; k <neurons[i].length ; k++) {
                     neurons[i][k].changeweights(aggregateWeights[i][k]);
+                    neurons[i][k].changebias(aggregateBiases[i][k]);
                 }
             }
-            //finally it will rewrite each neuron to match the average
-
+//            if(oldpercent<(100 * percent / imgs.length)) {
+//
+//                oldpercent=100 * percent / imgs.length;
+//            }
+            System.out.println(100 * percent / imgs.length);
+            percent=0;
         } //repeat
+
         try {
-            savestate("results");
+            savestate("SavedStates\\results");
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
     }
 
-    public int[] guessthatnumber(BufferedImage img){
+    public double[] guessthatnumber(BufferedImage img){
         //reads in the img into the first layer
 
         int whereWeAt = 0;
@@ -296,12 +304,12 @@ public class neural_Net {
                 int red = (rgb >> 16) & 0x000000FF;
                 int green = (rgb >>8 ) & 0x000000FF;
                 int blue = (rgb) & 0x000000FF;
-
-                rgb = (red + green + blue);
                 if (rgb == 765){
                     rgb =0;
                 }
-                neurons[0][whereWeAt].setValue((red + green + blue));
+                float luminance = (red * 0.2126f + green * 0.7152f + blue * 0.0722f)-128;
+                //System.out.println(luminance);
+                neurons[0][whereWeAt].setValue(luminance);
 
                 whereWeAt++;
             }
@@ -315,7 +323,7 @@ public class neural_Net {
         }
         //finds the hieghest of the out put nodes and returns what it believes teh number is
         int highest=-1;
-        Neuron highestnueron=neurons[layers-1][0];
+        Neuron highestnueron=new Neuron(-1000,outputnodes);
         for (int i = 0; i <neurons[layers-1].length ; i++) {
             if(highestnueron.getValue() < neurons[layers-1][i].getValue()){
                 highestnueron=neurons[layers-1][i];
@@ -323,7 +331,7 @@ public class neural_Net {
             }
         }
         //returns the number it believes the image to be as well as its certainty
-        int[] out ={highest,(int)highestnueron.getValue()*100};
+        double[] out ={highest,highestnueron.getValue()*100};
         return out;
     }
 
@@ -332,16 +340,22 @@ public class neural_Net {
 class Neuron{
     private double value;
     protected double[] weights;
-    protected double[] bias;
+    protected double bias;
     //maybe it should have a list of nodes from previous layer. maybe
     //comment from Owen: Is it necessary to have both weights and biases?
     public Neuron(double val, int previouslayer){
 
         weights=new double[previouslayer];
-        bias=new double[previouslayer];
+        bias=0;
         value=sigmoid(val);
     }
+    public void scramble(){
+        for (int i = 0; i <weights.length ; i++) {
+            weights[i]=(Math.random())-0.5;
 
+        }
+        bias=(Math.random());
+    }
     //last resort for evolution
     protected void randomMutation(){
 
@@ -357,23 +371,20 @@ class Neuron{
 
 
     public void changeweights(double [] w){
-        for(int i=0;i<w.length;i++){
+        for (int i = 0; i <w.length; i++) {
             weights[i]+=w[i];
         }
     }
 
 
-    public void changebias(double [] b){
-        for(int i=0;i<b.length;i++){
-            bias[i]+=b[i];
-        }
+    public void changebias(double b){
+        bias=b;
     }
 
-
     //Owen's homemade constructor, for making a neuron when you know about it
-    public Neuron(double[] weights, double[] biases, int val){
+    public Neuron(double[] weights, double bias, int val){
         this.weights = weights;
-        this.bias = biases;
+        this.bias = bias;
         value = sigmoid(val);
     }
 
@@ -393,20 +404,19 @@ class Neuron{
         return request;
     }
 
-
     public double[] requestsneurons(double change,Neuron[] previousnodes){//changes the weight of a neuron tahn returns requests for previous layer neurons
         double[] requests=new double[previousnodes.length];
         for (int i = 0; i <previousnodes.length ; i++) {
-            requests[i]=previousnodes[i].value+weights[i]*change;
+            requests[i]=sigmoid(previousnodes[i].getValue()+weights[i]*change);
         }
         return requests;
     }
     public void calculateval(Neuron[] neurons){
         double val=0;
         for(int i= 0;i<weights.length;i++){
-            val+=neurons[i].value*weights[i]+bias[i];
+            val+=neurons[i].value*weights[i];
         }
-        value=sigmoid(val);
+        value=sigmoid(val+bias);
     }
     public static double sigmoid(double x) {
         return (1/( 1 + Math.pow(Math.E,(-1*x))));
--- src/tantum_Numerus_Main.java
+++ src/tantum_Numerus_Main.java
@@ -2,32 +2,34 @@ import java.awt.*;
 import java.io.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 import java.util.Scanner;
 import javax.imageio.ImageIO;
 public class tantum_Numerus_Main {
     public static final int WIDTH=20;
     public static final int HEIGHT=20;
     public static final int SHADING=3; //used only for the benefit of the human looking at the resized images.
-    public static void imageFixer(String file){
-        File f=null;
-        BufferedImage img=null;
-        try{
-            f = new File("Images\\"+file+".png");
-            img = ImageIO.read(f);
-        }catch(IOException e){
-            System.out.println(e.getMessage());
-        }
-        BufferedImage resized=resize(img);
-        grayScale(resized);
-        //maybe put images in a folder in the git repository? then we don't need paths to places on your computer
-        try{
-            f = new File("C:\\Users\\NerdMachine2\\IdeaProjects\\tantumerous\\Images\\"+file+"_output.png");
-            ImageIO.write(resized, "png", f);
-        }catch(IOException e){
-            System.out.println(e.getMessage());
-        }
-    }
-    private static void grayScale(BufferedImage image){
+//    public static void imageFixer(String file){
+//        File f=null;
+//        BufferedImage img=null;
+//        try{
+//            f = new File("Images\\"+file+".png");
+//            img = ImageIO.read(f);
+//        }catch(IOException e){
+//            System.out.println(e.getMessage());
+//        }
+//        BufferedImage resized=resize(img);
+//        grayScale(resized);
+//        //maybe put images in a folder in the git repository? then we don't need paths to places on your computer
+//        try{
+//            f = new File("C:\\Users\\NerdMachine2\\IdeaProjects\\tantumerous\\Images\\"+file+"_output.png");
+//            ImageIO.write(resized, "png", f);
+//        }catch(IOException e){
+//            System.out.println(e.getMessage());
+//        }
+//    }
+    public static void grayScale(BufferedImage image){
         int width = image.getWidth();
         int height = image.getHeight();
         for(int y = 0; y < height; y++){
@@ -46,7 +48,7 @@ public class tantum_Numerus_Main {
         }
     }
 
-    private static BufferedImage resize(BufferedImage img){
+    public static BufferedImage resize(BufferedImage img){
         Image tmp = img.getScaledInstance(WIDTH, HEIGHT,Image.SCALE_SMOOTH);
         BufferedImage resized = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = resized.createGraphics();
@@ -61,7 +63,69 @@ public class tantum_Numerus_Main {
 
         Scanner in = new Scanner(System.in);
         String testOrTrain = in.next();
+        if(testOrTrain.equals("traingood")){
+            String lastGenState = "SavedStates/lastGenState.txt";
+            ArrayList<String> fileNames = new ArrayList<>();
+            ArrayList<Integer> fileVals = new ArrayList<>();
+            int lastSuccess = 0;
+            int currentSuccess =0;
+            System.out.println("how many images of each type would you like");
 
+            int num= in.nextInt();
+            for (int i = 0; i <2 ; i++) {
+                File folder = new File("data_set/"+i);
+                File[] listOfFiles = folder.listFiles();
+                for (int k = 0; k < num; k++) {
+                    if (listOfFiles[k].isFile()) {
+                        fileNames.add(i+"/"+listOfFiles[k].getName());
+                        fileVals.add(i);
+                    }
+                }
+            }
+            Random rand=new Random();
+            rand.setSeed(1);
+            Collections.shuffle(fileNames,rand);
+            rand.setSeed(1);
+            Collections.shuffle(fileVals,rand);
+            neural_Net hellaBigBrain = new neural_Net(2,20,20);
+            //hellaBigBrain.loadState("SavedStates/saveStateInUse.txt");
+            hellaBigBrain.savestate(lastGenState);
+
+            //training loop
+                currentSuccess=0;
+                int properOut;
+                double[] givenOut;
+                BufferedImage currentImage = ImageIO.read(new File("data_set/one1.png"));
+                Integer[] expout=new Integer[fileVals.size()];
+                String[] filelist=new String[fileNames.size()];
+                fileNames.toArray(filelist);
+                fileVals.toArray(expout);
+                System.out.println( fileNames.size());
+                hellaBigBrain.backprop(filelist,expout);
+                for (int i = 0; i < fileNames.size(); i++){
+
+                    properOut = fileVals.get(i);
+
+                    try {
+                        currentImage = ImageIO.read(new File("data_set/" + fileNames.get(i)));
+                    } catch (Exception ex){
+                        System.out.println("broke at file: " + fileNames.get(i));
+                    }
+                    grayScale(currentImage);
+                    currentImage = resize(currentImage);
+
+                    givenOut = hellaBigBrain.guessthatnumber(currentImage);
+
+                    System.out.println("num: " + givenOut[0]+" certainty:"+givenOut[1]+"% expected:"+properOut);
+
+                    if(givenOut[0] == properOut){
+                        currentSuccess++;
+                    }
+
+                }
+            System.out.println("I did " + currentSuccess + " wins today, thanks dad that's "+((double)currentSuccess/fileNames.size())*100+"%");
+
+        }
 
         //trains the NN with Owen's homemade training algorithm: do random sh1t till it works :D
         if(testOrTrain.equals("train")){
@@ -91,7 +155,7 @@ public class tantum_Numerus_Main {
                 hellaBigBrain.randomMutate();
                 currentSuccess=0;
                 int properOut;
-                int[] givenOut;
+                double[] givenOut;
                 BufferedImage currentImage = ImageIO.read(new File("data_set/one1.png"));
 
                 for (int i = 0; i < 50; i++){
@@ -139,7 +203,7 @@ public class tantum_Numerus_Main {
 
         //idk what this should do
         if(testOrTrain.equals("test")){
-
+            System.out.println("what file would you like me to look at?");
         }
 
     }
